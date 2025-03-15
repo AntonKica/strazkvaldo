6108abba575565bab60041078d9bf169501b97fc
Anton Kica <antonkica@pm.me>
2025-03-15 19:11:20 +0100

    [week-4] Setup Svelte dev environment

---
+++ strazkvaldo-ui/src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { base } from '$app/paths';
+	let { children } = $props();
+</script>
+
+<style>
+	ul#menu li{
+		display : inline;
+	}
+</style>
+
+
+<ul id="menu">
+	<li><a href="{base}/">Domov</a></li> |
+	<li><a href="{base}/about">O aplikácií</a></li>
+</ul>
+
+{@render children()}
---
+++ strazkvaldo-ui/src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	let count = $state(0);
+
+	function onclick() {
+		count++;
+	}
+</script>
+
+<h1>Stražkvaldo</h1>
