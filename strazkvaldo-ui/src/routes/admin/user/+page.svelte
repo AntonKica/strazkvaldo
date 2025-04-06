b59f99c8c5c64e1efb17eccb480157727b5ffa43
Anton Kica <antonkica@pm.me>
2025-04-06 12:28:53 +0200

    [week7] Added more attributes to admin's user list page

--- strazkvaldo-ui/src/routes/admin/user/+page.svelte
+++ strazkvaldo-ui/src/routes/admin/user/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import type { PageProps } from './$types';
     import { base } from '$app/paths';
+	import {app_user_role_code_to_string, app_user_role_to_string} from '$lib/common';
 	let { data }: PageProps = $props();
     console.log(data);
 </script>
@@ -16,18 +17,20 @@
             <td>používateľské meno</td>
             <td>rola</td>
             <td>vytvorený</td>
+            <td>upravený</td>
             <td></td>
         </tr>
     </thead>
     <tbody>
-{#each data.app_users as app_user}
-		<tr>
-            <td>{app_user.code}</td>
-            <td>{app_user.username}</td>
-            <td>{app_user.app_user_role}</td>
-            <td>{app_user.created}</td>
-            <td><a href="{base}/admin/user/{app_user.code}/view">pozri</a></td>
-        </tr>
-{/each}
+    {#each data.app_users as app_user}
+    <tr>
+        <td>{app_user.code}</td>
+        <td>{app_user.username}</td>
+        <td>{app_user_role_code_to_string(app_user.app_user_role)}</td>
+        <td>{app_user.created}</td>
+        <td>{app_user.updated}</td>
+        <td><a href="{base}/admin/user/{app_user.code}/view">pozri</a></td>
+    </tr>
+    {/each}
     </tbody>
 </table>
